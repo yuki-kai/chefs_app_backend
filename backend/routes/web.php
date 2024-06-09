bd93039f3a9953646f74f1f2ef7b5bb3b415369b
石原 祐希 <yuki.ishihara@A372.local>
2024-06-09 12:17:05 +0900

    Github Actionsの動作確認

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return ['Laravel' => app()->version(), '動作確認' => 'OK'];
 });
 
 require __DIR__.'/auth.php';
